89a3372ed0326456949fe96591bbdc6e9b5320c2
danielwetenkamp <daniel.wetenkamp@gmail.com>
2026-02-19 18:17:15 -0500

    LoginScreen.kt build (w/o Supabase) in ui package. tied in with App.kt

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -14,36 +15,32 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.compose_multiplatform
+import ui.LoginScreen
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        var isLoggedIn by remember { mutableStateOf(false) }
+
+        if(isLoggedIn) {
+            Text(text = "Home Screen Placeholder",
+                //MaterialTheme will allow for auto adjust for dark/light themes
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(top = 48.dp, bottom = 48.dp))
+        } else {
+            LoginScreen(
+                onLoginClick = { email, password -> isLoggedIn = true
+                },
+                onCreateAccountClick = {
+                    //will navigate to sign up screen
                 }
-            }
+            )
         }
     }
 }
